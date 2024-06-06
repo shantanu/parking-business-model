d02255cc24d0daec43b17f539c256243f0e93136
Shantanu Laghate <shantanu.laghate8@gmail.com>
2024-06-05 23:16:36 -0400

    format and fix location adding (#8)
    * format and fix location adding
    
    * format
    
    * cleanup

--- main.py
+++ main.py
@@ -1,130 +1,93 @@
-
 from results import generate_dataframe
 from model_params import *
 
 
 MONTHS: int = 60
 FIXED_COST: int = 100000
-SALES_COMMISSION: float = .25
-DISCOUNT_RATE: float = .10
+SALES_COMMISSION: float = 0.25
+DISCOUNT_RATE: float = 0.10
 
 
 # Parking Operator Model Parameters
 parking_operator_model_params: ModelParams = ModelParams(
     months=MONTHS,
     location_model_params=GrowthModelParams(
-        first_six_months_locations=0,
-        next_six_months_locations=0,
-        total_locations=100,
+        first_six_months_locations=1,
+        next_six_months_locations=1,
+        total_locations=80,
     ),
     pricing_params=PricingParams(
-        location_license_fee=350,
-        gateway_license_fee=75,
-        camera_license_fee=0.75
+        location_license_fee=1, gateway_license_fee=1, camera_license_fee=1
     ),
     gateways_per_location=10,
-    cameras_per_gateway=30
+    cameras_per_gateway=30,
 )
 
-cost_params: CostParams = CostParams(
-    cost_per_month=FIXED_COST
-)
+cost_params: CostParams = CostParams(cost_per_month=FIXED_COST)
 
-sales_params: SalesParams = SalesParams(
-    commission_rate=SALES_COMMISSION
-)
+sales_params: SalesParams = SalesParams(commission_rate=SALES_COMMISSION)
 
 valuation_params: ValuationParams = ValuationParams(
-    years=MONTHS//12,
-    discount_rate=DISCOUNT_RATE
+    years=MONTHS // 12, discount_rate=DISCOUNT_RATE
 )
 
 generate_dataframe(
-    {
-        "Parking Operator": parking_operator_model_params
-    },
+    {"Parking Operator": parking_operator_model_params},
     cost_params,
     sales_params,
     valuation_params,
-    "Operator_Business_model_output.csv"
+    "Operator_Business_model_output.csv",
 )
 
 
 # Starter Model Parameters
 starter_model_params: ModelParams = ModelParams(
-    months = MONTHS,
-    gateways_per_location = 5,
-    cameras_per_gateway = 30,
+    months=MONTHS,
+    gateways_per_location=5,
+    cameras_per_gateway=30,
     partner_params=PartnerParams(
-        num_partners=5,
-        max_locations=10,
-        months_between_partners=1
+        num_partners=5, max_locations=10, months_between_partners=1
     ),
     location_model_params=LinearModelParams(
-        first_month_of_location=3,
-        months_between_location=5
+        first_month_of_location=3, months_between_location=5
     ),
     pricing_params=PricingParams(
-        location_license_fee = 500,
-        gateway_license_fee = 100,
-        camera_license_fee = 0
-    )
+        location_license_fee=500, gateway_license_fee=100, camera_license_fee=0
+    ),
 )
 
 
 # Advanced Model Parameters
 advanced_model_params: ModelParams = ModelParams(
-    months = MONTHS,
-    gateways_per_location = 10,
-    cameras_per_gateway = 70,
+    months=MONTHS,
+    gateways_per_location=10,
+    cameras_per_gateway=70,
     partner_params=PartnerParams(
-        num_partners=10,
-        max_locations=25,
-        months_between_partners=2
+        num_partners=10, max_locations=25, months_between_partners=2
     ),
     location_model_params=LinearModelParams(
-        first_month_of_location=3,
-        months_between_location=3
+        first_month_of_location=3, months_between_location=3
     ),
     pricing_params=PricingParams(
-        location_license_fee = 350,
-        gateway_license_fee = 75,
-        camera_license_fee = 0
-    )
+        location_license_fee=350, gateway_license_fee=75, camera_license_fee=0
+    ),
 )
 
 
 # Enterprise Model Parameters
 enterprise_model_params: ModelParams = ModelParams(
-    months = MONTHS,
-    gateways_per_location = 15,
-    cameras_per_gateway = 100,
+    months=MONTHS,
+    gateways_per_location=15,
+    cameras_per_gateway=100,
     partner_params=PartnerParams(
-        num_partners=5,
-        max_locations=40,
-        months_between_partners=1
+        num_partners=5, max_locations=40, months_between_partners=1
     ),
     location_model_params=LinearModelParams(
-        first_month_of_location=3,
-        months_between_location=3
+        first_month_of_location=3, months_between_location=3
     ),
     pricing_params=PricingParams(
-        location_license_fee = 200,
-        gateway_license_fee = 50,
-        camera_license_fee = 0
-    )
-)
-
-generate_dataframe(
-    {
-        "Starter": starter_model_params,
-        "Advanced": advanced_model_params,
-        "Enterprise": enterprise_model_params
-    },
-    cost_params,
-    sales_params,
-    valuation_params,
-    "Partner_Business_model_output.csv"
+        location_license_fee=200, gateway_license_fee=50, camera_license_fee=0
+    ),
 )
 
 generate_dataframe(
@@ -132,10 +95,22 @@ generate_dataframe(
         "Starter": starter_model_params,
         "Advanced": advanced_model_params,
         "Enterprise": enterprise_model_params,
-        "Operator": parking_operator_model_params
     },
     cost_params,
     sales_params,
     valuation_params,
-    "Combined_Business_model_output.csv"
+    "Partner_Business_model_output.csv",
+)
+
+generate_dataframe(
+    {
+        "Starter": starter_model_params,
+        "Advanced": advanced_model_params,
+        "Enterprise": enterprise_model_params,
+        "Operator": parking_operator_model_params,
+    },
+    cost_params,
+    sales_params,
+    valuation_params,
+    "Combined_Business_model_output.csv",
 )
--- valuation.py
+++ valuation.py
@@ -1,17 +1,26 @@
 from model_params import ValuationParams
 
 
-def get_cash_flow(fee, commission, cost):
-    return fee - commission - cost
+def get_cash_flow(sales, commission, cost):
+    return sales - commission - cost
+
 
 def discount(amount, rate, years):
-    return (amount / ((1 + rate)**years))
+    return amount / ((1 + rate) ** years)
+
 
 def format_money(money: float) -> str:
-    return f'${round(money):,}'
+    return f"${round(money):,}"
 
-def get_yearly_valuation(valuation_params: ValuationParams, monthly_cumulative_cash_flow: list[float]) -> list[float]:
+
+def get_yearly_valuation(
+    valuation_params: ValuationParams, monthly_cumulative_cash_flow: list[float]
+) -> list[float]:
     return [
-        discount(monthly_cumulative_cash_flow[year*12-1], valuation_params.discount_rate, year)
-        for year in range(1, valuation_params.years+1)
+        discount(
+            monthly_cumulative_cash_flow[year * 12 - 1],
+            valuation_params.discount_rate,
+            year,
+        )
+        for year in range(1, valuation_params.years + 1)
     ]
